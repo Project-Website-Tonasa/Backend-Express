1123f21d94869ad4af2f663d7c0dcd4f3be6b7cb
@brillianita <brillianitaaa@gmail.com>
2023-03-07 13:29:13 +0800

    add auth for all routes

--- src/routes/laporanRoutes.js
+++ src/routes/laporanRoutes.js
@@ -7,21 +7,21 @@ const authJwt = require('../middleware/authUser');
 const router = Router();
 
 // KONTRAKTOR FEATURE
-router.get('/laporan/:noProyek', laporanHandler.getLaporanByNoProyekKont);
-router.post('/laporan/tambah', uploadFile.upload.single('file'), laporanHandler.createLaporan);
-router.post('/lapHarian/tambah', laporanHandler.createLapHarian);
-router.get('/detailLaporan/:id', laporanHandler.getLaporanDetail);
-router.get('/detaillapHarian/:id', laporanHandler.getDetailLapHarian);
+router.get('/laporan/:noProyek', [authJwt.verifyToken, authJwt.isAdminOrStafOrKontraktor], laporanHandler.getLaporanByNoProyekKont);
+router.post('/laporan/tambah', [authJwt.verifyToken, authJwt.isAdminOrStafOrKontraktor], uploadFile.upload.single('file'), laporanHandler.createLaporan);
+router.post('/lapHarian/tambah', [authJwt.verifyToken, authJwt.isAdminOrStafOrKontraktor], laporanHandler.createLapHarian);
+router.get('/detailLaporan/:id', [authJwt.verifyToken, authJwt.isAdminOrStafOrKontraktor], laporanHandler.getLaporanDetail);
+router.get('/detaillapHarian/:id', [authJwt.verifyToken, authJwt.isAdminOrStafOrKontraktor], laporanHandler.getDetailLapHarian);
 router.put('/detaillapHarian/:id', [authJwt.verifyToken, authJwt.isAdminOrStafOrKontraktor], laporanHandler.editDetailLapHarian);
 router.put('/laporan/edit/:id', [authJwt.verifyToken, authJwt.isAdminOrStafOrKontraktor], uploadFile.upload.single('file'), laporanHandler.updateLaporan);
 
 // STAFF OR ADMIN FEATURE
-router.get('/laporan', laporanHandler.getAllLaporan);
+router.get('/laporan', [authJwt.verifyToken, authJwt.isAdminOrStaff], laporanHandler.getAllLaporan);
 router.put('/laporanReview/edit/:id', [authJwt.verifyToken, authJwt.isAdminOrStaff], laporanHandler.updateStat);
 router.delete('/laporan/:id', [authJwt.verifyToken, authJwt.isAdminOrStaff], laporanHandler.deleteLaporan);
 router.put('/bast/:noProyek', [authJwt.verifyToken, authJwt.isAdminOrStafOrKontraktor], laporanHandler.updateBastStatus);
 router.get('/download/:name', [authJwt.verifyToken, authJwt.isAdminOrStaff], laporanHandler.download);
-router.get('/preview/:name', laporanHandler.previewPdf);
+router.get('/preview/:name', [authJwt.verifyToken, authJwt.isAdminOrStaff], laporanHandler.previewPdf);
 // router.get('/testingpup', pup.test);
 
 module.exports = router;
