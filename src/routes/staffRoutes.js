020940b8a698f0e210b1d3e80fb7c9ed1bc4f70c
@brillianita <brillianitaaa@gmail.com>
2023-03-11 22:13:17 +0800

    Fix the auth for get staff

--- src/routes/staffRoutes.js
+++ src/routes/staffRoutes.js
@@ -6,7 +6,7 @@ const router = Router();
 
 router.post('/staff/tambah', [authJwt.verifyToken, authJwt.isAdmin], staffHandler.createStaff);
 router.get('/staff', [authJwt.verifyToken, authJwt.isAdmin], staffHandler.getAllStaff);
-router.get('/staff/:id', [authJwt.verifyToken, authJwt.isAdmin], staffHandler.getStaffById);
+router.get('/staff/:id', [authJwt.verifyToken, authJwt.isAdminOrStaff], staffHandler.getStaffById);
 router.delete('/staff/:id', [authJwt.verifyToken, authJwt.isAdmin], staffHandler.deleteStaff);
 router.put('/staff/:id', [authJwt.verifyToken, authJwt.isAdminOrStaff], staffHandler.updateStaff);
 
