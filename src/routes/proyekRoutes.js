709ac065b8e831734cc35fc8be5387e847cdf711
@brillianita <brillianitaaa@gmail.com>
2023-06-06 13:21:54 +0800

    FIX: the condition WHERE in getAllProyek

--- src/routes/proyekRoutes.js
+++ src/routes/proyekRoutes.js
@@ -1,11 +1,11 @@
 const { Router } = require('express');
-const laporanHandler = require('../handler/proyekHandler');
+const proyekHandler = require('../handler/proyekHandler');
 const authJwt = require('../middleware/authUser');
 
 const router = Router();
 
-router.get('/proyek', [authJwt.verifyToken, authJwt.isAdminOrStaff], laporanHandler.getAllProyek);
-router.get('/proyek/:idUser', [authJwt.verifyToken, authJwt.isAdminOrStafOrKontraktor], laporanHandler.getProyekByIdKontraktor);
-router.get('/detproyek/:noProyek', [authJwt.verifyToken, authJwt.isAdminOrStafOrKontraktor], laporanHandler.getProyekByNoProyek);
+router.get('/proyek', [authJwt.verifyToken, authJwt.isAdminOrStaff], proyekHandler.getAllProyek);
+router.get('/proyek/:idUser', [authJwt.verifyToken, authJwt.isAdminOrStafOrKontraktor], proyekHandler.getProyekByIdKontraktor);
+router.get('/detproyek/:noProyek', [authJwt.verifyToken, authJwt.isAdminOrStafOrKontraktor], proyekHandler.getProyekByNoProyek);
 
 module.exports = router;
